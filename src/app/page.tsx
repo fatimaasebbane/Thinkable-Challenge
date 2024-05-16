766345328706985f474c6dfbb9b2bafe14147f93
fatima asebbane <fatimaasebbane2002@gmail.com>
2024-05-16 20:46:04 +0200

    add CRUD

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,31 @@
-require('dotenv').config();
+import Link from 'next/link'
+import React from 'react'
+import Item from './page_item'
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    
-       <h1>
-         first modification
-       </h1>
-      
-    </main>
-  );
+const getPosts = async () => {
+    // Because this is server components, we have to define the URL with http
+    const res = await fetch(process.env.BASE_URL + '/posts', { next: { revalidate: 0 } })
+
+    // Define the output to json, because if only res, it will return by any type
+    const json = await res.json()
+    return json
 }
+
+const Page = async () => {
+
+    const posts = await getPosts()
+
+    return (
+        <div className='w-[1200px] mx-auto py-20'>
+            <Link href={"/post/create"} className='px-3 py-2 bg-zinc-900 hover:bg-zinc-800 rounded-md text-white'>Create</Link>
+
+            <div className='grid grid-cols-3 gap-5 mt-8'>
+                {posts?.posts?.map((post: any, i: number) => (
+                    <Item key={i} post={post} />
+                )).sort().reverse()}
+            </div>
+        </div>
+    )
+}
+
+export default Page
