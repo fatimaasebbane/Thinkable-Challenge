bacb0a1da80db497b2ca5e4218db6af06688bd27
fatima asebbane <fatimaasebbane2002@gmail.com>
2024-05-17 12:06:15 +0200

    modify home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,9 +17,11 @@ const Page = async () => {
 
     return (
         <div className='w-[1200px] mx-auto py-20'>
+            
             <Link href={"/pages/create"} className='btn_danger'>Create new post</Link>
             <br></br>
-            <br></br><br></br><br></br>
+            <br></br><br>
+            </br><br></br>
             <div className='grid grid-cols-3 gap-5 mt-8'>
                 {posts?.posts?.map((post: any, i: number) => (
                     <Item key={i} post={post} />
