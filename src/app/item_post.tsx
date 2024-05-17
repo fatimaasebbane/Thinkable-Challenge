e7108d953fa769dc4a13d090ca244c86920c7f38
fatima asebbane <fatimaasebbane2002@gmail.com>
2024-05-17 12:13:40 +0200

    modify update function

---
+++ src/app/item_post.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import React from 'react';
+import { Post } from '@prisma/client/wasm';
+import { useRouter } from 'next/navigation';
+import { AiOutlineEdit, AiOutlineDelete } from 'react-icons/ai';
+import './styles/item.css';
+
+interface Props {
+    post: Post;
+}
+
+const Item: React.FC<Props> = ({ post }) => {
+    const router = useRouter();
+
+    const handleDelete = async (id: number) => {
+        await fetch('/posts?id=' + id, {
+            method: 'DELETE'
+        });
+
+        router.refresh();
+    };
+
+    return (
+        <div className="blog_post">
+            <div className="img_pod">
+                <img src="https://pbs.twimg.com/profile_images/890901007387025408/oztASP4n.jpg" alt="random image" />
+            </div>
+            <div className="container_copy">
+                <h3>{post.createdAt}</h3>
+                <h1>{post.title}</h1>
+                <p>{post.content}</p>
+                <a className="btn_primary" onClick={() => router.push(`/pages/show/${post.id}`)}>Read More</a>
+                <div className='flex justify-end mt-4'>
+                    <button
+                        className='bg-transparent border-none cursor-pointer outline-none'
+                        onClick={() => router.push(`/pages/update/${post.id}`)}
+                    >
+                        <AiOutlineEdit size={30} className='text-green-500 hover:text-blue-900 transition duration-200' />
+                    </button>
+                    <button
+                        className='bg-transparent border-none cursor-pointer outline-none ml-3'
+                        onClick={() => handleDelete(post.id)}
+                    >
+                        <AiOutlineDelete size={30} className='text-red-500 hover:text-red-600 transition duration-200' />
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Item;
---
+++ src/app/pages/show/[id]/page.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'next/navigation'; // Notez l'utilisation de useParams
+import { Post } from '@prisma/client/wasm';
+import '../../../styles/show.css'
+
+const ShowPost: React.FC = () => {
+    const { id } = useParams(); // Récupère l'ID à partir des paramètres de l'URL
+    const [post, setPost] = useState<Post | null>(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        if (id) {
+            const fetchPost = async () => {
+                try {
+                    const response = await fetch(`/posts/${id}`);
+                    const data = await response.json();
+                    setPost(data.post);
+                    setLoading(false);
+                } catch (error) {
+                    console.error("Error fetching post:", error);
+                    setLoading(false);
+                }
+            };
+            fetchPost();
+        }
+    }, [id]);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (!post) {
+        return <div>Post not found</div>;
+    }
+
+    return (
+        <div>
+        <a href='https://fonts.googleapis.com/css?family=Roboto:400,500,300,700' rel='stylesheet' type='text/css'></a>
+        <div className="wrapper">
+          <div className="top">
+            <div className="title"><h1> </h1></div></div>
+          <div className="content">
+            <div className="card first">
+              <h1 className="title_post">{post.title}</h1>
+              <p className="date">{post.createdAt}</p>
+              <p className="text">
+                {post.content}
+                </p>
+              </div>
+          </div>
+        </div>
+        </div>
+    );
+};
+
+export default ShowPost;
